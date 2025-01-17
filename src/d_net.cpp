40a45d3a062f6fe1426824053b509a8e7d87cb6f
hh79 <hammerhead1979@outlook.com>
2025-01-17 23:26:19 +0100

    Rework uncapped framerate in menus
    (cherry picked from commit 9eca5a275e5a44b3c18b8e08b5849f00fbbb160a)

--- src/d_net.cpp
+++ src/d_net.cpp
@@ -72,6 +72,7 @@
 #include "d_main.h"
 #include "i_interface.h"
 #include "savegamemanager.h"
+#include "hw_vrmodes.h"
 
 EXTERN_CVAR (Int, disableautosave)
 EXTERN_CVAR (Int, autosavecount)
@@ -1884,6 +1885,12 @@ void TryRunTics (void)
 
 	if (vid_dontdowait && ((vid_maxfps > 0) || (vid_vsync == true)))
 		doWait = false;
+
+	auto vrmode = VRMode::GetVRMode();
+	if (vrmode->IsVR())
+	{
+		doWait = false;
+	}
 
 	// get real tics
 	if (doWait)
